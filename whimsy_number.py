f278ad02b33b29cb4fe11fb4fda927ab47cd6664
scriubuguri <scriubuguri@tragdate.ninja>
2023-12-28 22:52:42 +0200

    customizable animation for welcome message

--- whimsy_number.py
+++ whimsy_number.py
@@ -2,21 +2,27 @@
 #this is a guessing game
 
 import random
+import time 
+
+def print_welcome_animation(message, delay=0.05):
+    for char in message:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()
 
 def whimsy_number():
-    print("Hello there! Welcome to the Whimsy Number Game!")
     player_name = input("What is your name? ")
     print(f"Hello, {player_name}! Nice to see you!")
     player_input = input("Do you want to play the Whimsy Number Game? (y/n)")
 
     if player_input == "y":
         print("Great! Let's play!")
-        print("I'm thinking of a number between 1 and 20. Can you guess it?")
+        print("I'm thinking of a number between 1 and 10. Can you guess it?")
     else:
-        print("Ohh, okey! See you next time.")
+        print("Ohh, okey.. See you next time XD")
 
     while player_input == "y":
-        number = random.randint(1, 20)
+        number = random.randint(1, 10)
         guess = int(input("Enter your guess: "))
         if guess < number:
             print("Too low. Try again.")
@@ -25,6 +31,8 @@ def whimsy_number():
         else:
             print(f"Congratulations, {player_name}! You guessed the whimsy number!")
             player_input = input("Do you want to play again? (y/n)")
-    
-    
+
+
+welcome_message = "Hello! Welcome to the Whimsy Number Game!"
+print_welcome_animation(welcome_message)
 whimsy_number() 
