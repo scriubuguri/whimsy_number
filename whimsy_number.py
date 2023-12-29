b71cdd876e8add40db7d275be90d624b8199140b
scriubuguri <scriubuguri@tragdate.ninja>
2023-12-29 23:25:22 +0200

    add scoring&levels to whimsy number game

--- whimsy_number.py
+++ whimsy_number.py
@@ -11,6 +11,7 @@ def print_welcome_animation(message, delay=0.05):
     print()
 
 def whimsy_number():
+    scores = 0
     player_name = input("What is your name? ")
     print(f"Hello, {player_name}! Nice to see you!")
     player_input = input("Do you want to play the Whimsy Number Game? (y/n)")
@@ -30,9 +31,23 @@ def whimsy_number():
             print("Too high. Try again.")
         else:
             print(f"Congratulations, {player_name}! You guessed the whimsy number!")
+            scores += 1
             player_input = input("Do you want to play again? (y/n)")
+            print(f"Your score is {scores}")
 
+        if scores > 2:
+            print("Congratulations! You are now moving to the next level. Guess a number between 1 and 20.")
+            new_number = random.randint(1, 20)
+            new_guess = int(input("Enter your guess: "))
+            if new_guess < new_number:
+                print("Too low. Try again.")
+            elif new_guess > new_number:
+                print("Too high. Try again.")
+            else:
+                print(f"Congratulations, {player_name}! You guessed the whimsy number!")
 
-welcome_message = "Hello! Welcome to the Whimsy Number Game!"
-print_welcome_animation(welcome_message)
-whimsy_number() 
+while True:
+    welcome_message = "Hello! Welcome to the Whimsy Number Game!"
+    print_welcome_animation(welcome_message)
+    whimsy_number() 
+ 
